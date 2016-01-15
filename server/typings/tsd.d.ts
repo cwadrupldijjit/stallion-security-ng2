ee847937054ca04f527cc9794fbb00fdfbb9b05b
Samuel Skeen <sskeen@sskeendevelopment.onmicrosoft.com>
2016-01-15 14:54:08 -0700

    installing type definition files for easier backend scripting and tsc

---
+++ server/typings/tsd.d.ts
@@ -0,0 +1,15 @@
+
+/// <reference path="express/express.d.ts" />
+/// <reference path="mime/mime.d.ts" />
+/// <reference path="node/node.d.ts" />
+/// <reference path="serve-static/serve-static.d.ts" />
+/// <reference path="helmet/helmet.d.ts" />
+/// <reference path="passport/passport.d.ts" />
+/// <reference path="passport-local/passport-local.d.ts" />
+/// <reference path="gulp/gulp.d.ts" />
+/// <reference path="orchestrator/orchestrator.d.ts" />
+/// <reference path="q/Q.d.ts" />
+/// <reference path="express-session/express-session.d.ts" />
+/// <reference path="mongoose/mongoose.d.ts" />
+/// <reference path="bcryptjs/bcryptjs.d.ts" />
+/// <reference path="body-parser/body-parser.d.ts" />
